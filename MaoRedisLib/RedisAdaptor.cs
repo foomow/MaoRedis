1611b23898033639c4eadeda914ff77628f7aeb9
MaoDaShu <foomow@163.com>
2019-10-20 22:46:49 +0800

    home updates

--- MaoRedisLib/RedisAdaptor.cs
+++ MaoRedisLib/RedisAdaptor.cs
@@ -20,12 +20,14 @@ namespace MaoRedisLib
             _password = password;
             _current_db_number = 0;
             Logger.Info("Redis initialized");
+
         }
 
         public bool Connect()
         {
             ConfigurationOptions options = ConfigurationOptions.Parse(_connectionString);
-            options.Password = _password;
+            if (_password != "")
+                options.Password = _password;
             options.AllowAdmin = true;
             options.ConnectTimeout = 30000;
             options.ResponseTimeout = 30000;
@@ -52,14 +54,14 @@ namespace MaoRedisLib
                 .Replace("databases", "")
                 .Replace(" ", "");
             int.TryParse(CountStr, out int ret);
-            Logger.Warn(ret.ToString());
             return ret;
         }
 
-        public void UseDB(int db_number)
+        public int UseDB(int db_number)
         {
             _current_db = Redis.GetDatabase(db_number);
-            _current_db_number = db_number;
+            _current_db_number = _current_db.Database;
+            return _current_db.Database;
         }
 
         public RedisKey[] GetKeys()
@@ -67,6 +69,49 @@ namespace MaoRedisLib
             _server = Redis.GetServer(_connectionString);
             RedisKey[] keys = _server.Keys(_current_db_number).ToArray();
             return keys;
+        }
+
+        public string Get(string key)
+        {
+            if (KeyType(key).ToString() == "String")
+                return _current_db.StringGet(key);
+            else
+                return $"({KeyType(key).ToString()})";
+        }
+
+        public HashEntry[] GetHash(string key)
+        {
+            return _current_db.HashGetAll(key);
+        }
+
+        public void SetHash(string key, HashEntry[] fields)
+        {
+            _current_db.HashSet(key, fields);
+        }
+
+        public RedisValue[] GetListAll(string key)
+        {
+            return _current_db.ListRange(key);
+        }
+
+        public RedisValue GetListByIndex(string key, int index = 0)
+        {
+            return _current_db.ListGetByIndex(key, index);
+        }
+
+        public bool Set(string key, string value)
+        {
+            return _current_db.SetAdd(key, value);
+        }
+
+        public bool Del(string key)
+        {
+            return _current_db.KeyDelete(key);
+        }
+
+        public RedisType KeyType(string key)
+        {
+            return _current_db.KeyType(key);
         }
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,16 +9,19 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            RedisAdaptor adaptor = new RedisAdaptor("13.231.216.183:6379", "MukAzxGMOL2");
+            Console.WriteLine("************** Hello Chengdu! **************");
+            RedisAdaptor adaptor = new RedisAdaptor("192.168.3.90:6379");
             adaptor.Connect();
-            adaptor.GetDBCount();
-            adaptor.UseDB(0);
+            int db_count=adaptor.GetDBCount();
+            Logger.Info($"Database count:{db_count}");
+            int current_db=adaptor.UseDB(0);
+            Logger.Info($"Current Database:{current_db}");
             RedisKey[] keys = adaptor.GetKeys().ToArray();
             foreach (RedisKey key in keys)
             {
-                Logger.Warn(key);
+                Logger.Info(adaptor.KeyType(key)+ " "+key.ToString()+":["+adaptor.Get(key)+"]");
             }
+            Console.WriteLine("************** Bye! **************");
         }
     }
 }
